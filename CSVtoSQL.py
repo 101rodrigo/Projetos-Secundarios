012ef86771a4a894fdaee6e15ff3701a7f776ad7
rodr-igo <110899040+101rodrigo@users.noreply.github.com>
2023-09-13 16:30:22 -0300

    Update CSVtoSQL.py

--- CSVtoSQL.py
+++ CSVtoSQL.py
@@ -1,2 +1,22 @@
 import csv
+from sqlite3 import Cursor
 import MySQLdb
+#MySQLdb Linux installing: sudo apt-get install python-mysqldb
+
+mydb = MySQLdb.coonect(host='localhost', user='exemple', passwd='exemp', db='exemp')
+cursor = mydb.cursor()
+cont = 1
+csv_data = csv.reader(file('exemple.csv'))
+
+
+for row in csv_data:
+  try:
+    cursor.execute('INSERT int TableExemple VALUES(%s, %s, %s, $s)', row)
+    print("Cadastro", cont, "realizado com sucesso!")
+    cont = cont + 1
+  except MySQLdb.Error as e:
+    print("Erro ao inserir o cadastro :/", cont)
+    
+mydb.commit()
+cursor.close()
+    
