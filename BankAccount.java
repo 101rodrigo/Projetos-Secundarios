ecc0222dc5114bca9421a579742605a58a9f451d
rodr-igo <110899040+Im-sat@users.noreply.github.com>
2023-09-13 01:49:01 -0300

    Update BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,19 @@
+public class Bank{
+  String Nome;
+  double Saldo;
+  int Idade;
+
+  //Constructor
+  public Bank(String Nome, double Saldo, int Idade) {
+    this.Nome = Nome;
+    this.Saldo = Saldo;
+    this.Idade = Idade;
+  }
+  //Getters and Setters
+  public String getNome() {
+    return Nome;
+  }
+
+  
+
+}
